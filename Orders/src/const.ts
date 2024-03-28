d754c005a1ed8128e42064c5c2e260f914294ca1
galkreiser <galkreiser@gmail.com>
2024-03-28 18:30:51 +0200

    added rabbit (event->order) and (order->user)

--- Orders/src/const.ts
+++ Orders/src/const.ts
@@ -2,3 +2,4 @@ export const CREATE_ORDER = "/api/order";
 export const GET_USER_ORDERS = "/api/order/:userId";
 export const GET_USERS_BY_EVENT = "/api/order/users/:eventId";
 export const GET_EVENTS_BY_USER = "/api/order/events/:username";
+export const IS_LOCAL = false;
--- Orders/src/index.ts
+++ Orders/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { consumeMessages } from "./consumer.js";
+
 import {
   CREATE_ORDER,
   GET_USER_ORDERS,
@@ -14,13 +16,21 @@ import {
   getEventsByUserRoute,
 } from "./routes.js";
 
+import { PublisherChannel } from "./publisher.js";
+
+export const publisherChannel = new PublisherChannel();
+
 dotenv.config();
 
 let dbUri;
 
-dbUri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster2.zpgwucf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2`;
+const DBUSER = process.env.DBUSER || "galkreiser";
+const DBPASS = process.env.DBPASS || "bADRRlIAm7ke6K5N";
+
+dbUri = `mongodb+srv://${DBUSER}:${DBPASS}@cluster2.zpgwucf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2`;
 
 await mongoose.connect(dbUri);
+consumeMessages();
 
 const port = process.env.PORT || 3002;
 
